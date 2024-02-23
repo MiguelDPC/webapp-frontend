5db811cb7f916eb1f5ad537446a53a20e0b874f2
miguel <migueldelgadopc@gmail.com>
2024-02-23 01:20:10 -0500

    Set Up User Handling -User can now be added -Password is hashed before being saved in database -User can be authenticated

--- public/script.js
+++ public/script.js
@@ -1,10 +1,9 @@
-function submitForm() {
-  console.log("Ran");
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
+function Login() {
+  var login = document.getElementById("login").value;
+  var pass = document.getElementById("pass").value;
 
   //send data
-  fetch('http://localhost:4000/api', {
+  fetch('http://localhost:4000/login', {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -12,7 +11,7 @@ function submitForm() {
       "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT",
       "Accept": "application/json",
     },
-    body: JSON.stringify({ name: name, email: email})
+    body: JSON.stringify({ username: login, password: pass})
   })
   .then(response => response.json())
   .then(data => {
@@ -22,3 +21,29 @@ function submitForm() {
     console.error('Error:', error);
   });
 }
+
+function CreateUser() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  var email = document.getElementById("email").value;
+  var building = document.getElementById("building").value;
+  console.log(email+building);
+  fetch('http://localhost:4000/CreateUser', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin" : "*",
+      "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({ username:username, password:password, email:email, building:building})
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
+
